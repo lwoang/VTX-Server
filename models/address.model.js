75679f9a05b37d7810e2a836a1fa5f6ffbbecbac
Luong Tuan Hoang <hoanga2k16lp@gmail.com>
2025-07-29 13:45:50 +0700

    commit

---
+++ models/address.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    address_line : {
+        type : String,
+        default : ""
+    },
+    city : {
+        type : String,
+        default : ""
+    },
+    country : {
+        type : String
+    },
+    mobile : {
+        type : Number,
+        default : null
+    },
+    status : {
+        type : Boolean,
+        default : true
+    },
+    userId : {
+        type : mongoose.Schema.ObjectId,
+        default : ""
+    }
+},{
+    timestamps : true
+})
+
+const AddressModel = mongoose.model('address',addressSchema)
+
+export default AddressModel
